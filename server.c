cc6028b665bfde60ef0b776ea29acef7d71eebd1
ShanEverittM <shaneeverittmurphy@gmail.com>
2020-10-28 16:32:55 -0700

    cleanup :

--- server.c
+++ server.c
@@ -14,69 +14,60 @@
 void communicate(int);
 
 int main(int argc, char const *argv[]) {
-  int sock, connection, len;
+  int socket_fd, connection_fd, len;
   struct sockaddr_in serveraddr, clientaddr;
-  int opt = 1;
+  int addr_len = sizeof(struct sockaddr_in);
+  int res = 0;
 
-  len = sizeof(serveraddr);
-  sock = socket(AF_INET, SOCK_STREAM, 0); // create socket
-  if (sock == -1) {
+
+  socket_fd = socket(AF_INET, SOCK_STREAM, 0); // create socket
+  if (socket_fd == -1) {
     printf("Failed to create socket...\n");
-    exit(0);
-  }
-
-  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-  { 
-      perror("setsockopt"); 
-      exit(EXIT_FAILURE); 
+    return 0;
   }
   printf("Socket created successfully...\n");
-
-  bzero(&serveraddr, sizeof(serveraddr)); // set values to 0
 
   // setup IP address and PORT
   serveraddr.sin_family = AF_INET;
   serveraddr.sin_addr.s_addr = INADDR_ANY;
   serveraddr.sin_port = htons(PORT);
+  
 
   // bind socket to a given IP address
-  if ((bind(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr))) < 0) {
+  res = bind(socket_fd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+  if (res < 0) {
     printf("Failed to bind socket...\n");
-    close(sock);
-    exit(0);
+    close(socket_fd);
+    return 0;
   }
-  printf("Socket binded successfully...\n");
+  printf("Socket bound successfully...\n");
   
   // Set server to start listening for client
-  if (listen(sock, 1) == -1) {
+  res = listen(socket_fd, 1);
+  if (res == -1) {
     printf("Server failed to listen...\n");
-    close(sock);
-    exit(0);
+    close(socket_fd);
+    return 0;
   }
   printf("Server is listening...\n");
 
   // Accept data packet from a client
-  connection = accept(sock, (struct sockaddr*)&serveraddr, &len);
-  if(connection < 0) {
+  connection_fd = accept(socket_fd, (struct sockaddr*)&clientaddr, &addr_len);
+  if (connection_fd < 0) {
     printf("Failed to connect with client...\n");
-    close(sock);
-    exit(0);
+    close(socket_fd);
+    return 0;
   }
   printf("Client accepted...\n");
   
   char buffer[1024];
-  int valread = read(connection, buffer, 1024);
-  printf("%s\n", buffer);
-  char *response = "Message Reveived";
-  send(connection, response, strlen(response), 0);
-  printf("Responded\n");
+  int bytes_read = recv(connection_fd, buffer, 1024, 0);
+  printf("Got message from client: %s\n", buffer);
+  char * response = "Message Reveived";
+  send(connection_fd, response, strlen(response), 0);
+  printf("Responded with: %s\n", response);
+  
+  close(socket_fd); // close socket
   return 0;
-
-  // begin communicating with the client
-  communicate(connection);
-
-  close(sock); // close socket
-  exit(1);
 }
 
